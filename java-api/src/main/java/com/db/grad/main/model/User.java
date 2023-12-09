3a9d80bb8b5588d576cb856b89af34e0fbbcd1ba
Aniswar <50guptaaniswar@gmail.com>
2023-12-09 22:19:19 +0530

    Fixed issue with trades fetching

--- java-api/src/main/java/com/db/grad/main/model/User.java
+++ java-api/src/main/java/com/db/grad/main/model/User.java
@@ -14,7 +14,7 @@ import java.util.Set;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String name;
